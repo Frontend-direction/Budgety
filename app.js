59980f411fbc63d09ac4a7abccfda507c7ff0ac5
Vova Pankiv <vovapankiw@gmail.com>
2019-04-19 08:14:23 +0300

    added DOM strings aliases

--- app.js
+++ app.js
@@ -9,6 +9,25 @@ let budgetController = (function () {
 
 let UIController = (function () {
 
+  const DOMStrings = {
+    inputType:'.add__type',
+    inputDescription:'.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  }
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value,
+      }
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  };
 })();
 
 
@@ -16,9 +35,12 @@ let UIController = (function () {
 
 let controller = (function (budgetCtrl, UICtrl) {
 
+  let DOM = UIController.getDOMStrings();
+
   let ctrlAddItem = function () {
     // 1. Get the field input data
-
+    let input = UICtrl.getInput();
+    console.log(input)
     // 2.Add the item to the budget controller
 
     // 3.Add the item to the UI
@@ -26,9 +48,8 @@ let controller = (function (budgetCtrl, UICtrl) {
     //4. Calculate the budget
 
     // 5. Display the bufger on UI
-    console.log('it works')
   }  
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', (event) => {
     if (event.keyCode === 13 || event.which === 13) {
